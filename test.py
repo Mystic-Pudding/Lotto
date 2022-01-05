f6df0d5d7b31cebafb866f27f70581f675210749
Mystic-Pudding <totocaca@naver.com>
2022-01-06 00:42:04 +0900

    .

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from tensorflow.keras.models import load_model
 from flask import Flask, json, jsonify
+import tensorflow
+model = load_model("lotto.h5")
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello"
+    return str(tensorflow.__version__)
 
 if __name__ == '__main__':
     app.run(debug=True)
