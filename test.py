ad84e51767e92cc0840fa47b7e5e261e1d48aa3d
Mystic-Pudding <totocaca@naver.com>
2022-01-06 01:24:37 +0900

    .

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 from tensorflow.keras.models import load_model
-model = load_model("lotto.h5")
 from flask import Flask, jsonify
+import numpy as np
+def onehot(number):
+    returnarray = []
+    for i in range(len(number)):
+        b = np.zeros(47)
+        for k in range(6):
+            b[number[i][k]] = 1
+        returnarray.append(b)
+    return returnarray
+
+model = load_model("lotto.h5")
 app = Flask(__name__)
 
 @app.route('/')
