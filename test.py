df4c5c2436ebbe363805b538bebaddb06f0b21e6
Mystic-Pudding <totocaca@naver.com>
2022-01-06 02:14:25 +0900

    .

--- test.py
+++ test.py
@@ -1,53 +1,24 @@
-from tensorflow.keras.models import load_model
-from flask import Flask, jsonify
-import numpy as np
-import requests
-def find_number(numbers):
-    find_numbers = []
-    numbers = list(numbers[0])
-    for i in range(6):
-        tmp = max(numbers)
-        index = numbers.index(tmp)
-        if(index==0):
-            numbers[index] = -99
-            a = numbers.index(max(numbers))
-            find_numbers.append(a)
-            continue
-        numbers[index] = -99
-        find_numbers.append(index)
-    find_numbers.sort()
-    return find_numbers
-def onehot(number):
-    returnarray = []
-    for i in range(len(number)):
-        b = np.zeros(47)
-        for k in range(6):
-            b[number[i][k]] = 1
-        returnarray.append(b)
-    return returnarray
-
-
-def test_lottonumber_load(number):
-    lotto_numbers = []
-    url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='+str(number)
-    req_result = requests.get(url)
-    small_lotto_numbers = [] 
-    for i in range(6):
-            small_lotto_numbers.append(req_result.json()['drwtNo'+str(i+1)])
-    lotto_numbers.append(small_lotto_numbers)
-    
-    return lotto_numbers 
-
-model = load_model("lotto.h5")
-test = onehot(test_lottonumber_load(995))
-test = np.array(test)
-predict=model.predict(test)
-
+from tensorflow.keras.models import Sequential,load_model
+from tensorflow.keras.layers import Dense
+# model = Sequential()
+# train = [1,2,3]
+# target = [4,5,6]
+# model.add(Dense(1))
+# model.add(Dense(1))
+# model.compile(loss='mean_squared_error',optimizer='adam')
+# model.fit(train,target,epochs=10)
+# model.save("linear.h5")
+model = load_model("linear.h5")
+predict = model.predict([4,5,6])
+predict_array = []
+for i in range(3):
+    predict_array.append(int(predict[i][0]))
+from flask import Flask, json, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello"
+    return jsonify(array=predict_array)
 
 if __name__ == '__main__':
     app.run(debug=True)
